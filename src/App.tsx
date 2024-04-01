83d0b7a797cd79e0256f65ff7e0f96fdf192ad6d
Scoozy <79184720+scoooozy@users.noreply.github.com>
2024-03-31 21:00:23 -0700

    Cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import './App.css'
 function App() {
     return <div className='h-screen overflow-hidden' id='game'>
             <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+
     </div>
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,20 @@
 import clubs from "../assets/images/cards/clubs.png"
 const Card = () => {
     const number = 10
-    const suitsMiddle = []
-    for(let  i = 0; i < number; i++) {
-        suitsMiddle.push(
-            <img className="w-6 h-6" src={clubs} alt="Card suit" />
+    const suitsMiddle = [[], [], []]
+    for (let i = 0; i < 4; i++) {
+        suitsMiddle[0].push(
+            0
+        )
+    }
+    for (let i = 0; i < 2; i++) {
+        suitsMiddle[1].push(
+            0
+        )
+    }
+    for (let i = 0; i < 4; i++) {
+        suitsMiddle[2].push(
+            0
         )
     }
     return (
@@ -13,8 +23,22 @@ const Card = () => {
                 <h3 className="font-bold text-2xl">10</h3>
                 <p><img className="w-5 h-5" src={clubs} alt="Card suit" /></p>
             </div>
-            <div className="grow ">
-                {suitsMiddle}
+            <div className="grow flex items-center justify-between content-between">
+                <div>
+                    {suitsMiddle[0].map(pack => (
+                        <img className="w-7 h-7 mt-3" src={clubs} alt="Card suit" />
+                    ))}
+                </div>
+                <div className="flex flex-col justify-between gap-10">
+                    {suitsMiddle[1].map(pack => (
+                        <img className="w-7 h-7" src={clubs} alt="Card suit" />
+                    ))}
+                </div>
+                <div>
+                    {suitsMiddle[2].map(pack => (
+                        <img className="w-7 h-7  mt-3" src={clubs} alt="Card suit" />
+                    ))}
+                </div>
             </div>
             <div className="flex rotate-180 justify-start items-center flex-col">
                 <h3 className="font-bold text-2xl">10</h3>
